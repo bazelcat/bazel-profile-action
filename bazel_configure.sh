24b7afab948a5506d67445b25c33b6311325ac67
bazelcommits <bazelcommits@bazelcat.com>
2023-09-17 14:05:07 -0700

    Implement bazelw

--- bazel_configure.sh
+++ bazel_configure.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+set -e
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 GITHUB_JOB=${GITHUB_JOB:-0}
 echo $GITHUB_RUNTIME_TOKEN >> "/tmp/${GITHUB_JOB}.action_env.sh"
+echo "BAZEL_PROFILE_ROOT=$SCRIPTPATH" >> "/tmp/${GITHUB_JOB}.action_env.sh"
 sudo mv /usr/local/bin/bazel /usr/local/bin/bazel.og
-sudo cp .github/actions/foobar/bazel /usr/local/bin/bazel
+sudo cp $SCRIPTPATH/bazelw /usr/local/bin/bazel
 sudo mv /usr/local/bin/bazelisk /usr/local/bin/bazelisk.og
-sudo cp .github/actions/foobar/bazel /usr/local/bin/bazelisk
+sudo cp $SCRIPTPATH/bazelw /usr/local/bin/bazelisk
