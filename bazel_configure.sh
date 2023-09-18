d6e565ba5b6b58fa7602e535182499fc39839fb5
bazelcommits <bazelcommits@bazelcat.com>
2023-09-18 05:54:24 -0700

    Fixes for bazel-re

--- bazel_configure.sh
+++ bazel_configure.sh
@@ -4,7 +4,14 @@ SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 GITHUB_JOB=${GITHUB_JOB:-0}
 echo $GITHUB_RUNTIME_TOKEN >> "/tmp/${GITHUB_JOB}.action_env.sh"
 echo "BAZEL_PROFILE_ROOT=$SCRIPTPATH" >> "/tmp/${GITHUB_JOB}.action_env.sh"
-sudo mv /usr/local/bin/bazel /usr/local/bin/bazel.og
-sudo cp $SCRIPTPATH/bazelw /usr/local/bin/bazel
-sudo mv /usr/local/bin/bazelisk /usr/local/bin/bazelisk.og
-sudo cp $SCRIPTPATH/bazelw /usr/local/bin/bazelisk
+BAZEL=$(which bazel)
+echo "Original bazel $BAZEL"
+
+BAZELISK=$(which bazelisk)
+echo "Original bazelisk $BAZELISK"
+printenv
+
+sudo mv $BAZEL $BAZEL.og
+sudo cp $SCRIPTPATH/bazelw $BAZEL
+sudo mv $BAZELISK $BAZELISK.og
+sudo cp $SCRIPTPATH/bazelw $BAZELISK
